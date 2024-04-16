33baac8da84dc8c2f48c0425e8c4dffaabb2930c
aibunny <waihenyafredrick@gmail.com>
2024-04-16 21:22:44 +0300

    Update: cleaned agriscrapper_data ready for mart modelling

--- amis_dbt/models/intermediate/refined/ref_commodities.sql
+++ amis_dbt/models/intermediate/refined/ref_commodities.sql
@@ -1,31 +1,45 @@
 select
     id as commodity_id,
     commodity,
+    supply_volume::numeric as supply_volume,
+    
     case 
         when classification = '-' or classification is null then 'not applicable' 
         else classification 
     end as classification,
+    
     case 
         when grade = '-' or grade is null then 'not applicable' 
         else grade 
     end as grade,
+    
     case 
         when sex = '-' or sex is null then 'not applicable' 
         else sex 
     end as sex,
+    
     case 
         when split_part(wholesale, '/', 1) = ' - ' then null
         else split_part(wholesale, '/', 1)::numeric 
-    end as wholesale_amount,
-    split_part(wholesale, '/', 2) as wholesale_unit,   
+    end as wholesale_price,
+    
+    case 
+        when split_part(wholesale, '/', 2) = ' - ' then null
+        else split_part(wholesale, '/', 2)
+    end as wholesale_unit,   
     case 
         when split_part(retail, '/', 1) = ' - ' then null
         else split_part(retail, '/', 1)::numeric 
-    end as retail_amount,  
-    split_part(retail, '/', 2) as retail_unit,
+    end as retail_price,  
+
+    case 
+        when split_part(retail, '/', 2) = ' - ' then null
+        else split_part(retail, '/', 2) 
+    end as retail_unit,  
+
     county,
-    date
-    
+    date::date as date
+
 from 
     {{ref('stg_amis__commodities')}}
      
